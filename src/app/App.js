9cc92dbd99e81c40a8e5de09cbaeebc946b02988
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2024-11-13 16:44:56 +0100

    test

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import Portfolio from '../pages/Portfolio/Portfolio';
@@ -8,18 +8,24 @@ import Footer from '../components/Footer/Footer';
 
 function App() {
   return (
-    <BrowserRouter basename="/happyclicks">
+    
+    <Router
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
       <Header />
       <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       </div>
       <Footer />
-    </BrowserRouter>
+    </Router>
   );
 }
 
