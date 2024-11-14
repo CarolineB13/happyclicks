365f569338b441a309f5ead0a003603080e50144
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2024-11-14 12:26:00 +0100

    changements des puces dans services

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,15 +2,16 @@
 import React from 'react';
 import './Services.css';
 import TechnologiesChart from '../TechnologiesChart/TechnologiesChart';
+import arrowIcon from '../../assets/arrow-icon.svg';
 
 function Services() {
   return (
     <section className="services-section">
       <h2>Mes Compétences</h2>
       <ul className="custom-list">
-        <li>Création de sites vitrines</li>
-        <li>Responsive Design</li>
-        <li>Développement front-end (HTML, CSS, JavaScript, React)</li>
+        <li><img src={arrowIcon} alt="Flèche" className="arrow-icon" />Création de sites vitrines</li>
+        <li><img src={arrowIcon}  alt="Flèche" className="arrow-icon" />Responsive Design</li>
+        <li><img src={arrowIcon}  alt="Flèche" className="arrow-icon" />Développement front-end (HTML, CSS, JavaScript, React)</li>
       </ul>
 
       {/* Section des Compétences Techniques avec Barres de Progression */}
